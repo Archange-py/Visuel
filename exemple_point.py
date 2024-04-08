8b4ca4cf9ec539c583b2606f0152f554b882bd16
Archange <archange_paradise@proton.me>
2024-04-08 14:03:23 +0200

    ajout d'exemples pour le fichier point

--- exemple_point.py
+++ exemple_point.py
@@ -1,5 +1,21 @@
-from Visuel import Point, draw_points
+from Visuel import Point, Screen, draw_points, set_pixel
+
+print(Screen.style.keys())
 
 A, B, C = Point(50, 50), Point(100, 100), Point(200, 80)
 
-draw_points([A, B, C], 'black', True)
+#draw_points([A, B, C], 'black', True)
+
+def exemple(x,y,color):
+  for s in Screen.style.keys(): 
+    draw_points([Point(x,y)],color,style=s)
+    set_pixel(x,y,"black")
+    x += 20
+
+exemple(100,60,"green")
+exemple(100,90,"red")
+exemple(100,120,"blue")
+
+#exemple(100,60,"yellow")
+#exemple(100,90,"magenta")
+#exemple(100,120,"cyan")
