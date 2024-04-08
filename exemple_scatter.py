4174a3c2714eee1aca806334ed515a724f18ac80
Archange <archange_paradise@proton.me>
2024-04-08 14:02:18 +0200

    ajout d'exemples pour le fichier scatter

--- exemple_scatter.py
+++ exemple_scatter.py
@@ -1,9 +1,37 @@
+from random import randint
 from Visuel import *
+from math import sin
+
+print(Screen.style.keys())
 
 #   Exemple 1
-Screen.center = Point(50,200)
+#Screen.center = Point(50,200)
+#
+#X = [3, 29, 55, 81, 108, 133, 160]
+#Y = [109, 144, 160, 161, 148, 122, 78]
+#
+#scatter(X, Y, "red", "O")
 
-X = [3, 29, 55, 81, 108, 133, 160, 165]
-Y = [109, 144, 160, 161, 148, 122, 78]
+#   Exemple 2
+#Screen.center = Point(160,111)
+#
+#X = [x for x in range(-100, 100, 5)]
+#Y = [sin(y*25)*30 for y in range(-100, 100, 5)]
+#
+#scatter(X, Y, "red", "*")
 
-scatter(X, Y, "red", "O")
+#   Exemple 3
+#Screen.center = Point(160,111)
+#
+#X = [randint(-100, 80) for x in range(-100, 100, 10)]
+#Y = [randint(-100, 80) for y in range(-100, 100, 10)]
+#
+#scatter(X, Y, "red", "+")
+
+#   Exemple 4
+Screen.center = Point(160,111)
+
+X = [x for x in range(-100, 100, 3)]
+Y = [sin(y*25)*30 for y in range(-100, 100, 3)]
+
+scatter(X, Y, "red", ".")
