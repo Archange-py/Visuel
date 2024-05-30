b9f63fab49bc864ea6827c4587158e667e8a3d2c
Archange <archange_paradise@proton.me>
2024-05-30 09:56:14 +0200

    Ajout d'un fichier d'exemple et d'images

---
+++ example_fractale.py
@@ -0,0 +1,35 @@
+from fractal import Point, tree, pi
+from kandinsky import fill_rect
+from time import sleep
+
+# Basic example
+
+#tree(position=Point(160,170),length=50,angle=pi/2,iteration=12,thickness=1,color=[(88,41,0), (0,255,0)],left_angle=pi/4,right_angle=pi/4,ratio=0.7)
+#tree(position=Point(160,170),length=50,angle=pi/2,iteration=12,thickness=1,color="black",left_angle=pi/4,right_angle=pi/4,ratio=0.7)
+#tree(position=Point(160,170),length=50,angle=pi/2,iteration=12,thickness=1,color=(0,0,0),left_angle=pi/4,right_angle=pi/4,ratio=0.7)
+#tree(position=Point(160,170),length=50,angle=pi/2,iteration=12,thickness=1,color=["black","green"],left_angle=pi/4,right_angle=pi/4,ratio=0.7)
+
+# Animation example
+
+def animation():
+  a1, a2 = pi/4, pi/4
+  while True:
+    tree(position=Point(160,170),length=50,angle=pi/2,iteration=12,thickness=1,color=["black","cyan"],left_angle=a1,right_angle=a2,ratio=0.7)
+    sleep(1)
+    fill_rect(0,0,320,222,"white")
+    a1 += 1 ; a2 -= 1
+
+#animation()
+
+# Most popular tree
+
+#tree(position=Point(160,210),length=50,angle=pi/2,iteration=12,thickness=3,color=["red","yellow"],left_angle=2*pi/20,right_angle=2*pi/20,ratio=0.75)
+#tree(position=Point(160,170),length=40,angle=pi/2,iteration=15,thickness=5,color=[(0,0,0), (255,0,255)],left_angle=2*pi/11,right_angle=2*pi/11,ratio=0.75)
+#tree(position=Point(160,210),length=100,angle=pi/2,iteration=12,thickness=1,color=["magenta","purple"],left_angle=pi/2,right_angle=pi/2,ratio=0.7)
+
+# Other tree
+
+#tree(position=Point(100,200),length=100,angle=pi/2,iteration=12,thickness=4,color=["black","magenta"],left_angle=pi/4+5,right_angle=pi/4,ratio=0.7)
+#tree(position=Point(160,210),length=65,angle=pi/2,iteration=12,thickness=4,color=["black","magenta"],left_angle=pi/4+12,right_angle=pi/4,ratio=0.7)
+#tree(position=Point(160,210),length=65,angle=pi/2,iteration=12,thickness=4,color=["black","magenta"],left_angle=pi/4+6,right_angle=pi/4,ratio=0.7)
+#tree(position=Point(160,210),length=70,angle=pi/2,iteration=12,thickness=4,color=["black","magenta"],left_angle=pi/4+13,right_angle=pi/4,ratio=0.7)
---
+++ fractal.py
@@ -0,0 +1,16 @@
+from visuel import Point, Screen, interpolate, draw_lines
+from math import pi, cos, sin
+import kandinsky as kd
+
+def tree(position: Point = Point(160,170), length: int = 40, angle: float | int = pi/2, iteration: int = 15, thickness: int = 1, color: list[str | tuple[int]] | tuple | str = Screen.palette['PrimaryColor'], left_angle: int | float = pi/4, right_angle: int | float = pi/4, ratio: int | float = 0.7):
+  degrad = True if isinstance(color, (list)) else False
+  if degrad: liste_color = interpolate(kd.color(color[0]), kd.color(color[1]), iteration+1)
+  N, e = 0, thickness/iteration
+  def _tree(position,length,angle,iteration,thickness,color,_n):
+    if iteration == 0: return
+    P2 = Point(position.x-cos(angle)*length,position.y-sin(angle)*length) ; round(P2) ; round(position)
+    if degrad: draw_lines([(position,P2)],[(kd.color(liste_color[_n]),kd.color(liste_color[_n+1]))],thickness)
+    else: draw_lines([(position,P2)],color,thickness)
+    _tree(P2,length*ratio,angle+right_angle,iteration-1,thickness-e if thickness!=1 else thickness,color,_n+1)
+    _tree(P2,length*ratio,angle-left_angle,iteration-1,thickness-e if thickness!=1 else thickness,color,_n+1)
+  return _tree(position,length,angle,iteration,thickness,color,N)
